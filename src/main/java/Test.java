b0892454d768b0afbd5fa439faab86db51ecd605
sbx_user1051 <sbx_user1051@169.254.41.197>
2023-12-30 16:15:36 +0000

    Sat Dec 30 16:15:36 UTC 2023

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Test {
+import java.util.HashMap;
+
+public class Test {
+    public static void main(String[] args)
+    {
+        System.out.println("hello");
+        checkout("ABBAACCD");
+
+    }
+
+    public static Integer checkout(String skus) {
+        int totalPrice = 0;
+
+        //Price for each SKU
+        HashMap<String, Integer> mapSKUsPrice = new HashMap<String, Integer>();
+        mapSKUsPrice.put("A", 50);
+        mapSKUsPrice.put("B", 30);
+        mapSKUsPrice.put("C", 20);
+        mapSKUsPrice.put("D", 15);
+
+        HashMap<String, Integer> mapSKUsCounter = new HashMap<String, Integer>();
+
+        for (int i = 0; i < skus.length(); i++)
+        {
+            char sku = skus.charAt(i);
+
+            if (mapSKUsCounter.get(sku) != null)
+            {
+
+            }
+            else {
+
+            }
+        }
+
+        return totalPrice;
+    }
 }
+
--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -33,8 +33,3 @@ public class CheckoutSolution {
         return totalPrice;
     }
 }
-
-
-
-
-

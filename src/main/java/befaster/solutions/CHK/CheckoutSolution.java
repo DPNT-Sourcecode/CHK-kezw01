46e9adc497a42ebd39f96fc64355a9652ac813eb
sbx_user1051 <sbx_user1051@169.254.41.197>
2023-12-30 16:06:36 +0000

    Sat Dec 30 16:06:36 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -6,14 +6,25 @@ import java.util.HashMap;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
+        int totalPrice = 0;
+
         HashMap<String, Integer> mapSKUsPrice = new HashMap<String, Integer>();
         mapSKUsPrice.put("A", 50);
         mapSKUsPrice.put("B", 30);
         mapSKUsPrice.put("C", 20);
         mapSKUsPrice.put("D", 15);
 
-        
+
+
+        for (int i = 0; i < skus.length(); i++)
+        {
+            char sku = skus.charAt(i);
+
+        }
+
+        return totalPrice;
     }
 }
+
 
 
